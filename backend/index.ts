40eef21d1b2aee6eeabf4ca16f3b08fba11b0181
parthrajveluri <parthrajveluri@yahoo.com>
2024-05-21 15:23:18 -0400

    Implemented Buyer authentication

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,3 @@
-
 import "dotenv/config";
 import mongoose from "mongoose";
 import { Buyer } from "./models/Buyers";
@@ -6,10 +5,10 @@ import { Seller } from "./models/Sellers";
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import compression from "compression";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import http from "http";
-import { router as buyerRouter } from "./routes/buyers";
+import { buyerRouter } from "./routes/buyers";
 
 const app = express();
 app.use(
@@ -20,26 +19,22 @@ app.use(
 app.use(cookieParser());
 app.use(compression());
 app.use(bodyParser.json());
-app.use(express.json());
-
-
-app.use("/api", buyerRouter);
-app.get("/", (req, res) => {
-  res.status(200).send("hello!");
-});
-
+app.use(bodyParser.urlencoded({extended: true}))
+app.use("/api/v1", buyerRouter);
 const server = http.createServer(app);
 
-const MONGO_URL:string = (process.env.MONGODB_URL as string);
-async()=>{
-    try{
-        await mongoose.connect(MONGO_URL)
-    }catch(err){
-        console.log("Error connecting to database")
-        console.log(err)
+const MONGO_URL: string = process.env.MONGODB_URL as string;
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGO_URL);
+    } catch (err) {
+     console.log("Error connecting to database");
+        console.log(err);
     }
-} 
+};
+connectDB();
 
+console.log("Database state: " + mongoose.connection.readyState)
 server.listen(8000, () => {
     console.log("Server running on http://localhost:8000");
 });
