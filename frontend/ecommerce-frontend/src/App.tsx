57284ff2f411c7c3ff541937688fccdad72c017f
parthrajveluri <parthrajveluri@yahoo.com>
2024-05-22 15:21:23 -0400

    Setup frontend routing

--- frontend/ecommerce-frontend/src/App.tsx
+++ frontend/ecommerce-frontend/src/App.tsx
@@ -1,8 +1,16 @@
+import { homedir } from "os";
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
 
 function App() {
-  return <div className="App">Hello</div>;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
