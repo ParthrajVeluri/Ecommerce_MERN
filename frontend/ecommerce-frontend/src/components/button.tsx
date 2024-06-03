2b6998b6b6eda62eb35aced6a2282fe368e3f1c6
parthrajveluri <parthrajveluri@yahoo.com>
2024-06-03 15:28:12 -0400

    made color changes

--- frontend/ecommerce-frontend/src/components/button.tsx
+++ frontend/ecommerce-frontend/src/components/button.tsx
@@ -5,10 +5,10 @@ import { cn } from "../utils/utils";
 const buttonVariants = cva("inline-flex items-center justify-center rounded-md text-sm border", {
     variants: {
         variant: {
-            default: "bg-nexus text-white",
+            default: "bg-nexus text-white hover:bg-nexus-shade-600",
         },
         size: {
-            default: "h-10 py-2 px-4 my-2",
+            default: "h-10 mx-2 py-2 px-4 my-2",
             sm: "h-9 px-2 rounded-md",
             lg: "h-11 px-8 rounded-md",
         },
--- frontend/ecommerce-frontend/tailwind.config.js
+++ frontend/ecommerce-frontend/tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
             colors: {
                 transparent: "transparent",
                 current: "currentColor",
-                nexus: "#50a060",
+                nexus: "#ffaa33",
+                "nexus-shade": {
+                    100: "#dcecdf",
+                    200: "#b9d9bf",
+                    300: "#96c6a0",
+                    400: "#73b380",
+                    500: "#50a060",
+                    600: "#40804d",
+                    700: "#30603a",
+                    800: "#204026",
+                    900: "#102013",
+                },
                 defaultBg: "#f2f2f2"
             },
             fontFamily: {
